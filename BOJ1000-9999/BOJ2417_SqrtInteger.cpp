7c26adde18bef201a6efc17b306ec9d4738a1306
dudnbubn <koqj1234@gmail.com>
2021-09-07 19:52:01 +0900

    20210907

---
+++ BOJ1000-9999/BOJ2417_SqrtInteger.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+	long long n, q;
+	cin >> n;
+	q = sqrt(n);
+	if (q * q == n)	cout << q;
+	else	cout << q + 1;
+	return 0;
+}
