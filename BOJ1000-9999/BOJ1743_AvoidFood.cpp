eb6bb2c399007f8ccc86bdd41ba289dfb9f43c7d
dudnbubn <koqj1234@gmail.com>
2021-09-28 14:53:07 +0900

    20210928

---
+++ BOJ1000-9999/BOJ1743_AvoidFood.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int n, m, k;
+bool path[101][101] = { 0, };
+bool visited[101][101] = { 0, };
+int di[4] = { -1,1,0,0 };
+int dj[4] = { 0,0,-1,1 };
+
+int BFS(int i, int j) {
+	int result = 0;
+	queue<pair<int, int>> q;
+	visited[i][j] = true;
+	q.push({ i,j });
+	int cur_i, cur_j, next_i, next_j;
+	while (!q.empty()) {
+		result++;
+		cur_i = q.front().first;
+		cur_j = q.front().second;
+		q.pop();
+		for (int k = 0; k < 4; k++) {
+			next_i = cur_i + di[k];
+			next_j = cur_j + dj[k];
+			if (next_i < 1 || next_j < 1 || next_i > n || next_j > m)
+				continue;
+			if (path[next_i][next_j] == 1 && !visited[next_i][next_j]) {
+				visited[next_i][next_j] = true;
+				q.push({ next_i, next_j });
+			}
+		}
+	}
+	return result;
+}
+
+int main() {
+	cin >> n >> m >> k;
+	int r, c;
+	for (int i = 0; i < k; i++) {
+		cin >> r >> c;
+		path[r][c] = 1;
+	}
+	int maximum = 0;
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= m; j++) {
+			if (path[i][j] == 1 && !visited[i][j])
+				maximum = max(maximum, BFS(i, j));
+		}
+	}
+	cout << maximum;
+	return 0;
+}
