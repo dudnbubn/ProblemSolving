dbafde4091c4262c0173de55e9feff9d6d7e6b97
dudnbubn <koqj1234@gmail.com>
2022-02-19 17:23:12 +0900

    BOJ11568 - 2022 02 19

---
+++ BOJ10000-99999/BOJ11568_Trick.cpp
@@ -0,0 +1,34 @@
+// key point : Longest Increasing Subsequence
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int n;
+int arr[1001];
+int cache[1001];
+
+int LIS() {
+	int index = 0;
+	cache[0] = arr[0];
+	for (int i = 1; i < n; i++) {
+		if (arr[i] > cache[index]) {
+			index++;
+			cache[index] = arr[i];
+		}
+		else {
+			int index2 = lower_bound(cache, cache + index, arr[i]) - cache;
+			cache[index2] = arr[i];
+		}
+	}
+	return index + 1;
+}
+
+int main() {
+	cin >> n;
+	for (int i = 0; i < n; i++)	cin >> arr[i];
+	cout << LIS();
+
+	return 0;
+}
