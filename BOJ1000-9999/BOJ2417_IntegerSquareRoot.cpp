d2a3b9e27e19b820b6dad8b5c92c103145a201e9
dudnbubn <koqj1234@gmail.com>
2022-03-12 01:37:41 +0900

    BOJ2417 - 2022 03 12

--- BOJ1000-9999/BOJ2417_IntegerSquareRoot.cpp
+++ BOJ1000-9999/BOJ2417_IntegerSquareRoot.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <cmath>
+
 using namespace std;
 
 int main() {
 	long long n, q;
 	cin >> n;
+	
 	q = sqrt(n);
-	if (q * q == n)	cout << q;
+	if (q * q >= n)	cout << q;
 	else	cout << q + 1;
 	return 0;
 }
