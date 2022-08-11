559e387bc3a4e7b4d6b4df9fb49858fb64d497f4
koqj1234 <koqj1234@gmail.com>
2022-08-11 23:40:24 +0900

    BOJ12738 & JAVA BOJ16637 - 2022 08 11

---
+++ BOJ10000-99999/BOJ12738_LongestIncreasingSubsequence3.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int N;
+int arr[1000000];
+int cache[1000000];
+
+int LIS() {
+	int index = 0;
+	cache[0] = arr[0];
+	for (int i = 1; i < N; i++) {
+		if (cache[index] < arr[i]) 
+			cache[++index] = arr[i];
+		else {
+			int index2 = lower_bound(cache, cache + index, arr[i]) - cache;
+			cache[index2] = arr[i];
+		}
+	}
+
+	return index + 1;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	cin >> N;
+	for (int i = 0; i < N; i++)	cin >> arr[i];
+
+	cout << LIS();
+
+	return 0;
+}
