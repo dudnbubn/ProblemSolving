67bca71a9a5e48df18cd914a183d275669f0087b
koqj1234 <koqj1234@gmail.com>
2022-10-15 18:20:54 +0900

    BOJ2086 - 2022 10 15

---
+++ BOJ1000-9999/BOJ2086_SumOfFibonacci.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+
+#define MOD 1000000000
+
+unordered_map<long long, long long> um;
+
+long long Fibo(long long N) {
+	if (N == 0)	return 0;
+	if (N <= 2)	return 1;
+	if (um.find(N) != um.end())	return um[N];
+
+	long long k;
+	if (N & 1) {
+		k = (N + 1) / 2;
+		return um[N] = ((Fibo(k) * Fibo(k) % MOD) + (Fibo(k - 1) * Fibo(k - 1) % MOD)) % MOD;
+	}
+	else {
+		k = N / 2;
+		return um[N] = ((2 * Fibo(k - 1) % MOD + Fibo(k)) % MOD) * Fibo(k) % MOD;
+	}
+}
+
+int main() {
+	long long a, b;
+	cin >> a >> b;
+
+	cout << (Fibo(b + 2) - Fibo(a + 1) + MOD) % MOD;
+	return 0;
+}
