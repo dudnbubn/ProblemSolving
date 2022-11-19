4528eaaba5004689edf52e9582d402de4e302a94
koqj1234 <koqj1234@gmail.com>
2022-11-20 00:10:55 +0900

    BOJ2747 update

--- BOJ1000-9999/BOJ2747_FibonacciNumber.cpp
+++ BOJ1000-9999/BOJ2747_FibonacciNumber.cpp
@@ -1,14 +1,2 @@
-#include <iostream>
-using namespace std;
-
-int f[46];
-
-int main() {
-	int N;
-	cin >> N;
-	f[0] = 0;
-	f[1] = 1;
-	for (int i = 2; i <= N; i++) 
-		f[i] = f[i - 1] + f[i - 2];
-	cout << f[N];
-}
+#include<iostream>
+using namespace std;int main(){int N,a=0,b=1;cin>>N;for(int i=1;i<N;i++)i%2?a+=b:b+=a;cout<<(N%2?b:a);}
