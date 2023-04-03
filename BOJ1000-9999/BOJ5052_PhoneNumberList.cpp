360800d65acacb48baa5e8124074fa409d9b34bf
dudnbubn <76838107+dudnbubn@users.noreply.github.com>
2023-04-03 23:10:20 +0900

    BOJ5052 - 2023 04 03

--- BOJ1000-9999/BOJ5052_PhoneNumberList.cpp
+++ BOJ1000-9999/BOJ5052_PhoneNumberList.cpp
@@ -3,38 +3,64 @@
 
 using namespace std;
 
-string arr[10000];
+#define MAX_N 10'000
+
+struct Node {
+	char number;
+	bool is_end;
+	Node* nexts[10];
+	Node(char number) {
+		this->number = number;
+		this->is_end = false;
+		for (int i = 0; i < 10; i++)
+			this->nexts[i] = nullptr;
+	}
+};
+
+string numbers[MAX_N];
+Node* starts[10];
+
+bool check(string s) {
+	int index = s[0] - '0';
+	if (starts[index] == nullptr)	starts[index] = new Node(s[0]);
+
+	Node* current = starts[index];
+	if (current->is_end)	return false;
+	int s_size = s.size();
+	for (int i = 1; i < s_size; i++) {
+		index = s[i] - '0';
+		if (current->nexts[index] == nullptr)	current->nexts[index] = new Node(s[i]);
+		current = current->nexts[index];
+		if (current->is_end)	return false;
+	}
+	current->is_end = true;
+
+	return true;
+}
+
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
 	cout.tie(nullptr);
 
-	int t;
-	cin >> t;
+	int TC;
+	cin >> TC;
 
-	int n;
-	while (t--) {
-		cin >> n;
-		for (int i = 0; i < n; i++)	cin >> arr[i];
-		sort(arr, arr + n);
+	int N;
+	while (TC--) {
+		cin >> N;
+		for (int i = 0; i < 10; i++)	starts[i] = nullptr;
+
+		for (int i = 0; i < N; i++)	cin >> numbers[i];
+		sort(numbers, numbers + N);
 		
-		bool flag = false;
-
-		for (int i = 0; i < n - 1; i++) {
-			for (int j = i + 1; j < n; j++) {
-				flag = false;
-				for (int k = 0; k < arr[i].size(); k++) {
-					if (arr[i][k] != arr[j][k]) {
-						flag = true;
-						break;
-					}
-				}
-				if (!flag)	break;
-			}
-			if (!flag)	break;
+		bool result = true;
+		for (int i = 0; i < N; i++) {
+			result &= check(numbers[i]);
 		}
-		if (flag)	cout << "YES\n";
+		
+		if (result)	cout << "YES\n";
 		else	cout << "NO\n";
 	}
 	return 0;
