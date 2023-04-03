0b04a4354d673ec88777484cef9d8501cbba2fb8
dudnbubn <76838107+dudnbubn@users.noreply.github.com>
2023-04-04 08:55:05 +0900

    BOJ3079 - 2023 04 04

--- BOJ1000-9999/BOJ3079_Immigration.cpp
+++ BOJ1000-9999/BOJ3079_Immigration.cpp
@@ -1,31 +1,35 @@
-//#include <iostream>
-//
-//using namespace std;
-//
-//int N, M;
-//int T[100001];
-//
-//int main() {
-//	long long start = 0, end = 0;
-//
-//	cin >> N >> M;
-//	for (int i = 0; i < N; i++) {
-//		cin >> T[i];
-//		end = end > T[i] ? end : T[i];
-//	}
-//	end *= M;
-//	
-//	long long cnt;
-//	while (start <= end) {
-//		long long mid = (start + end) / 2;
-//		cnt = 0;
-//		for (int i = 0; i < N; i++) 
-//			cnt += mid / T[i];
-//		
-//		if (cnt < M)	start = mid + 1;
-//		else	end = mid - 1;
-//	}
-//
-//	cout << start;
-//	return 0;
-//}
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+#define MAX_N 100'000
+
+int arr[MAX_N];
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int N, M;
+	cin >> N >> M;
+	for (int i = 0; i < N; i++)	cin >> arr[i];
+
+	unsigned long long start = 0, end = 1'000'000'000'000'000'001 / N;
+	unsigned long long mid;
+
+	unsigned long long cnt;
+	while (start < end) {
+		mid = (start + end) / 2;
+		
+		cnt = 0;
+		for (int i = 0; i < N; i++)
+			cnt += mid / arr[i];
+		
+		if (cnt >= M)	end = mid;
+		else	start = mid + 1;
+	}
+	cout << start;
+	return 0;
+}
