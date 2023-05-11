c98e011f750fa9659a948dba8e17756e41765fbc
dudnbubn <76838107+dudnbubn@users.noreply.github.com>
2023-05-11 13:17:07 +0900

    BOJ4355 - 2023 05 11

--- BOJ1000-9999/BOJ4355_RelativelyPrime.cpp
+++ BOJ1000-9999/BOJ4355_RelativelyPrime.cpp
@@ -1,39 +1,41 @@
 #include <iostream>
-#include <stack>
+#include <vector>
 
 using namespace std;
 
-stack<int> factors;
-
-int euler(int N) {
-	int temp = N, factor = 2;
-	while (factor <= temp) {
-		if (temp % factor == 0) {
-			if (factors.empty() || factors.top() != factor)	factors.push(factor);
-			temp /= factor;
+vector<int> get_prime_factors(int N) {
+	vector<int> result;
+	int temp_N = N;
+	for (int i = 2; i * i <= N; i++) {
+		if (temp_N % i == 0) {
+			result.push_back(i);
+			while (temp_N % i == 0)
+				temp_N /= i;
 		}
-		else
-			factor++;
 	}
-	
-	while (!factors.empty()) {
-		factor = factors.top();
-		factors.pop();
+	if (temp_N != 1)
+		result.push_back(temp_N);
+	return result;
+}
 
-		N = (N / factor) * (factor - 1);
-	}
-	return N;
+int get_euler_phi(int N) {
+	int result = N;
+	vector<int> prime_factors = get_prime_factors(N);
+
+	for (int prime_factor : prime_factors) 
+		result = result * (prime_factor - 1) / prime_factor;
+	return result;
 }
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
-	cout.tie(nullptr);
 
 	int N;
 	cin >> N;
-	while (N > 0) {
-		cout << euler(N) << "\n";
+	while (N) {
+		if (N == 1)	cout << "0\n";
+		else	cout << get_euler_phi(N) << "\n";
 		cin >> N;
 	}
 
