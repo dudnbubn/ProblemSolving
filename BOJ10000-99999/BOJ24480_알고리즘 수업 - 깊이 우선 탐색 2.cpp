d1be2ca70131bcd09d0300f3f79a44ef6742e9dc
koqj1234 <koqj1234@gmail.com>
2023-07-05 22:28:45 +0900

    BOJ24480 - 2023 07 05

---
+++ BOJ10000-99999/BOJ24480_알고리즘 수업 - 깊이 우선 탐색 2.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+#define MAX_N 100'000
+
+vector<int> next_nodes_of[MAX_N + 1];
+int index_of[MAX_N + 1];
+
+int index = 0;
+
+bool compare(int a, int b) {
+	return a > b;
+}
+
+void DFS(int current) {
+	index_of[current] = ++index;
+	for (int next_node : next_nodes_of[current]) {
+		if (index_of[next_node])	continue;
+		DFS(next_node);
+	}
+}
+
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int N, M, R;
+	cin >> N >> M >> R;
+
+	int u, v;
+	for (int m = 0; m < M; m++) {
+		cin >> u >> v;
+		next_nodes_of[u].push_back(v);
+		next_nodes_of[v].push_back(u);
+	}
+
+	for (int n = 1; n <= N; n++) {
+		sort(next_nodes_of[n].begin(), next_nodes_of[n].end(), compare);
+	}
+
+	DFS(R);
+
+	for (int n = 1; n <= N; n++)
+		cout << index_of[n] << "\n";
+	return 0;
+}
